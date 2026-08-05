d71dcab83c80f3f80911405f7f7720caf8301566
Priyanshu <punnu1366@gmail.com>
2026-08-05 13:34:15 +0530

    Add User model and Alembic migrations

--- app/config.py
+++ app/config.py
@@ -6,7 +6,6 @@ class Settings(BaseSettings):
     DATABASE_NAME:str
     DATABASE_USERNAME:str
     DATABASE_PASSWORD:str
-
     SECRET_KEY:str
     ALGORITHM:str
     ACCESS_TOKEN_EXPIRE_MINUTES:int
---
+++ app/models.py
@@ -0,0 +1,13 @@
+from datetime import datetime, timezone
+from sqlalchemy import Column, DateTime, Integer, String, func
+from app.database import Base
+
+
+class UsersTable(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password_hash = Column(String(128), nullable=False)
+    created_at = Column(DateTime, default=func.now(), nullable=False)
