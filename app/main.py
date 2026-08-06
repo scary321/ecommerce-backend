3ccddfae7d5750c47d2cc0d009a96b96990f58a3
Priyanshu <punnu1366@gmail.com>
2026-08-06 14:52:37 +0530

    user registration endpoint in main .py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session ,status
 from app.schemas import UserResponse
 from app.schemas import UserCreate
 from app.database import get_db
+from app.utils import hash_password
+from app.models import UsersTable
 
 app = FastAPI()
 
@@ -13,5 +15,13 @@ def root():
 
 @app.post("/users",status_code=status.HTTP_201_CREATED,response_model=UserResponse)
 def create_user(user: UserCreate,db: Session = Depends(get_db)):
+    new_user = UsersTable(
+        username=user.username,
+        email=user.email,
+        password_hash=hash_password(user.password)
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
 
-    return db_user
+    return new_user
