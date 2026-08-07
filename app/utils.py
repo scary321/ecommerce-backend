d3a0c69495a65a6e5f0e2dac4eb707c62f094d80
Priyanshu <punnu1366@gmail.com>
2026-08-07 15:09:57 +0530

    add jwt for create token for login

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,7 @@
 from passlib.context import CryptContext
+from datetime import datetime,timedelta
+from jose import JWTError,jwt
+from .config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -7,3 +10,14 @@ def hash_password(password: str):
 
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data:dict):
+    to_encode=data.copy()
+    
+    expire =datetime.now()+timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    to_encode.update({"exp":expire})
+    
+    encode_jwt =jwt.encode(to_encode,settings.SECRET_KEY,algorithm=settings.ALGORITHM)
+    
+    return encode_jwt
