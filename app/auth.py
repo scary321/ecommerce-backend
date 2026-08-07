5735d37acd4f798b3901fea3a86f258e1ea1ccb1
Priyanshu <punnu1366@gmail.com>
2026-08-08 03:24:11 +0530

    auth file for decode the payload and checking id is match or not

---
+++ app/auth.py
@@ -0,0 +1,26 @@
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends,HTTPException,status,Depends
+from app.database import get_db
+from sqlalchemy.orm import Session 
+from jose import JWTError,jwt
+from app import models
+from .config import settings
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+
+def get_current_user(token:str=Depends(oauth2_scheme),db: Session = Depends(get_db)):
+    
+    payload=jwt.decode(token,settings.SECRET_KEY,algorithms=[settings.ALGORITHM])
+    
+    user_id=payload.get("user_id")
+    
+    if user_id is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    
+    user = db.query(models.UsersTable).filter(models.UsersTable.id == user_id).first()
+    
+    if not user:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        
+    return user
+
