0e17d08985889a71e0c85485c98e42ecd869d1dc
Priyanshu <punnu1366@gmail.com>
2026-08-07 15:53:17 +0530

    adding login api andpoint for verify password and email and create a acces token

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI , Depends,status
+from fastapi import FastAPI , Depends, status ,HTTPException
 from sqlalchemy.orm import Session 
-from app.schemas import UserResponse
-from app.schemas import UserCreate
+from app.schemas import UserResponse ,UserCreate,UserLogin,LoginResponse
 from app.database import get_db
-from app.utils import hash_password
+from app.utils import hash_password,verify_password,create_access_token
 from app.models import UsersTable
+from app import models
+from jose import JWTError,jwt
+from .config import settings
+
 
 app = FastAPI()
 
@@ -25,3 +28,16 @@ def create_user(user: UserCreate,db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return new_user
+
+@app.post("/login",response_model=LoginResponse)
+def login_user(user_credential:UserLogin,db:Session = Depends(get_db)):
+    
+    user = db.query(models.UsersTable).filter(models.UsersTable.email == user_credential.email).first()
+    
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    if not verify_password(user_credential.password,user.password_hash):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    
+    access_token=create_access_token(data={"user_id":user.id})
+    return{"access_token":access_token,"token_type":"bearer"}
