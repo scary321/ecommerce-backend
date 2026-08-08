f608193c695b22945a96c73241f0e7cc8dab16fb
Priyanshu <punnu1366@gmail.com>
2026-08-08 18:10:28 +0530

    adding update endpoint for updating user

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI , Depends, status ,HTTPException
 from sqlalchemy.orm import Session 
-from app.schemas import UserResponse ,UserCreate,UserLogin,LoginResponse
+from app.schemas import UserResponse ,UserCreate,UserLogin,LoginResponse,UserUpdate
 from app.database import get_db
 from app.utils import hash_password,verify_password,create_access_token
 from app.models import UsersTable
@@ -9,6 +9,7 @@ from jose import JWTError,jwt
 from .config import settings
 from app.auth import get_current_user
 from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy import update
 
 
 app = FastAPI()
@@ -57,4 +58,28 @@ def get_user(id:int, current_user=Depends(get_current_user),db:Session = Depends
             detail="User not found"
         )
 
+    if (current_user.id != id):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="Access denied")
+    
+    return user
+
+@app.patch("/users/{id}",response_model=UserResponse)
+def update_user(id:int,user_update: UserUpdate, current_user=Depends(get_current_user),db:Session = Depends(get_db)):
+    if (current_user.id != id):
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="Access denied")
+    user = db.query(models.UsersTable).filter(models.UsersTable.id == id).first()    
+    
+    if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="User not found"
+            )
+    if user_update.email:
+        user.email = user_update.email  
+    if user_update.password:
+        user.password_hash =hash_password(user_update.password)
+        
+    db.commit()
+    db.refresh(user)
+    
     return user
