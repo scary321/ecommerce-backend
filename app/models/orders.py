2d7ce36c3cbd6e25c773e69a7ebe2575cea6d151
Priyanshu <punnu1366@gmail.com>
2026-08-15 19:14:43 +0530

    adding payment models and relation with ordertables

--- app/models/orders.py
+++ app/models/orders.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, DateTime, Integer, String, func,Float,ForeignKey
+from sqlalchemy import Column, DateTime, Integer, String, func,Numeric,ForeignKey
 from sqlalchemy.orm import relationship
 from app.database import Base
 
@@ -7,13 +7,15 @@ class OrderTable(Base):
     
     id=Column(Integer,primary_key=True)
     user_id=Column(Integer,ForeignKey("users.id", ondelete="CASCADE"),nullable=False)
-    total=Column(Float,nullable=False)
+    total=Column(Numeric,nullable=False)
     status=Column(String,nullable=False,default="pending")
     created_at=Column(DateTime, default=func.now(), nullable=False)
     
     user = relationship("UsersTable",back_populates="orders")
     
     items = relationship("OrderItemTable",back_populates="order",cascade="all, delete-orphan")
+    
+    payment = relationship("PaymentTable",back_populates="order",uselist=False,cascade="all, delete-orphan")
     
 class OrderItemTable(Base):
     __tablename__ = "order_items"
@@ -22,7 +24,7 @@ class OrderItemTable(Base):
     order_id=Column(Integer,ForeignKey("orders.id", ondelete="CASCADE"),nullable=False)
     product_id=Column(Integer,ForeignKey("products.id", ondelete="CASCADE"),nullable=False)
     quantity=Column(Integer,nullable=False)
-    price=Column(Float,nullable=False)
+    price=Column(Numeric,nullable=False)
     
     order = relationship("OrderTable",back_populates="items")
     
---
+++ app/models/payment.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column,Integer, String,Float,ForeignKey,Numeric
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+class PaymentTable(Base):
+    __tablename__="payments"
+    
+    id = Column(Integer,primary_key=True)
+    order_id = Column(Integer,ForeignKey("orders.id", ondelete="CASCADE"),nullable=False,unique=True)
+    amount = Column(Numeric,nullable=False)
+    payment_method = Column(String,nullable=False)
+    status=Column(String,nullable=False,default="pending")
+    transaction_id = Column(String,unique=True,nullable=True)
+    
+    order = relationship("OrderTable",back_populates="payment")
