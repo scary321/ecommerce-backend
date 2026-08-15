4dba66cd0ba24558248db8ab1da420e8acbee525
Priyanshu <punnu1366@gmail.com>
2026-08-15 20:44:16 +0530

    fixing some bugs

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from app.routers.cart import cart_router
 from fastapi import FastAPI
 from app.routers.orders import orders_router
 from app.routers.admin import admin_router
+from app.routers.payment import payment_router
 
 app = FastAPI()
 
@@ -12,3 +13,4 @@ app.include_router(admin_router)
 app.include_router(product_router)
 app.include_router(cart_router)
 app.include_router(orders_router)
+app.include_router(payment_router)
---
+++ app/models/__init__.py
@@ -0,0 +1,2 @@
+from app.models.orders import OrderTable
+from app.models.payment import PaymentTable
