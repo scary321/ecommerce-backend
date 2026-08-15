50d1c7e11f786ed97e32d5458f2014d52fbee1f0
Priyanshu <punnu1366@gmail.com>
2026-08-15 19:27:41 +0530

    adding payment schema

---
+++ app/schemas/payment.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, ConfigDict
+from decimal import Decimal
+
+class PaymentCreate(BaseModel):
+    order_id:int
+    payment_method:str
+    
+class PaymentResponse(BaseModel):
+    id:int
+    order_id:int
+    amount:Decimal
+    payment_method:str
+    status:str
+    transaction_id:str|None=None
+    
+    model_config=ConfigDict(from_attributes=True)
